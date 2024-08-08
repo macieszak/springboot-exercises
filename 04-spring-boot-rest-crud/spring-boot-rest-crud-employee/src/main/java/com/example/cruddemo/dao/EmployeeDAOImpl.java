fc9137b9b0d813650cdeee809ec421da0c4b3109
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-08-08 23:40:37 +0200

    REST CRUD APIs section.

---
+++ 04-spring-boot-rest-crud/spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/dao/EmployeeDAOImpl.java
@@ -0,0 +1,40 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    private EntityManager entityManager;
+
+    public EmployeeDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("FROM Employee", Employee.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+        entityManager.remove(employee);
+    }
+}
