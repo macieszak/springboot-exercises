0dfbb12ccdb4f2b7b52c6919207e54e6670e1985
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-08-15 17:55:38 +0200

    Spring MVC section, part 2.

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -1,20 +1,36 @@
 package com.example.thymeleafdemo.controller;
 
 import com.example.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
 
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
+        model.addAttribute("languages", languages);
+        model.addAttribute("systems", systems);
+
 
         return "student-form";
     }
